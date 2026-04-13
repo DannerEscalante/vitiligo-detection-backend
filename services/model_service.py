bd7b6dd70513d5aa78bf3de7e9a6cf30a3458aba
Danner Escalante <danner.escalante.noel@gmail.com>
2026-04-13 13:56:08 -0400

    mas errores

--- services/model_service.py
+++ services/model_service.py
@@ -1,24 +1,43 @@
 import tensorflow as tf
 import numpy as np
 import cv2
+from tensorflow.keras.applications import EfficientNetB0
 from tensorflow.keras.applications.efficientnet import preprocess_input
+from tensorflow.keras import layers, models
 
-MODEL_PATH = "models_ml/modelo_vitiligo_final.keras"
+MODEL_WEIGHTS = "models_ml/pesos_vitiligo.weights.h5"
 
 modelo = None
+
+
+def construir_modelo():
+    base_model = EfficientNetB0(weights='imagenet', include_top=False, input_shape=(224,224,3))
+    base_model.trainable = False
+
+    model = models.Sequential([
+        base_model,
+        layers.GlobalAveragePooling2D(),
+        layers.BatchNormalization(),
+        layers.Dropout(0.4),
+        layers.Dense(256, activation='relu'),
+        layers.Dropout(0.3),
+        layers.Dense(1, activation='sigmoid')
+    ])
+
+    return model
 
 
 def cargar_modelo():
     global modelo
     if modelo is None:
-        modelo = tf.keras.models.load_model(MODEL_PATH, compile=False)
+        modelo = construir_modelo()
+        modelo.load_weights(MODEL_WEIGHTS)
     return modelo
 
 
 def predecir_imagen(file):
     model = cargar_modelo()
 
-    # Leer imagen como en colab
     file.seek(0)
     file_bytes = np.frombuffer(file.read(), np.uint8)
     img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
@@ -26,24 +45,17 @@ def predecir_imagen(file):
     if img is None:
         raise Exception("No se pudo leer la imagen")
 
-    # Convertir a RGB
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    # Resize EXACTO
     img_resized = cv2.resize(img_rgb, (224, 224))
 
-    # Preprocesamiento EXACTO del colab
     img_input = preprocess_input(
         np.expand_dims(img_resized, axis=0).astype(np.float32)
     )
 
-    # Predicción
     prob = float(model.predict(img_input, verbose=0)[0][0])
 
-    # Lógica EXACTA del colab
-    umbral = 0.5
-
-    if prob >= umbral:
+    if prob >= 0.5:
         diagnostico = "VITÍLIGO DETECTADO"
         confianza = prob * 100
     else:
