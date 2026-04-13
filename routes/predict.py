c9751361082d0edc670825e197756a9136d8f460
Danner Escalante <danner.escalante.noel@gmail.com>
2026-04-13 11:33:11 -0400

    Integracion del modelo cnn

--- routes/predict.py
+++ routes/predict.py
@@ -1,17 +1,21 @@
 from fastapi import APIRouter, Depends, File, UploadFile
 from core.deps import obtener_usuario_actual
-
+from services.model_service import predecir_imagen
 
 router = APIRouter()
 
-
 @router.post("/predict")
-def predict(
+async def predict(
     imagen: UploadFile = File(...),
     usuario_id: str = Depends(obtener_usuario_actual)
 ):
-    
+
+    resultado = predecir_imagen(imagen.file)
+
+    diagnostico = "vitiligo" if resultado > 0.5 else "no vitiligo"
+
     return {
-        "mensaje": "Acceso permitido",
-        "usuario_id": usuario_id
+        "usuario_id": usuario_id,
+        "probabilidad": resultado,
+        "diagnostico": diagnostico
     }
---
+++ services/model_service.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+MODEL_PATH = "models_ml/modelo_vitiligo_final.h5"
+
+modelo = tf.keras.models.load_model(MODEL_PATH)
+
+
+
+def predecir_imagen(file):
+    img = Image.open(file).convert("RGB")
+    img = img.resize((224, 224))  # depende de tu modelo
+
+    img_array = np.array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    pred = modelo.predict(img_array)[0][0]
+
+    return float(pred)
