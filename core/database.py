5de41e100737f65826ce6b181143ff453b492d71
Danner Escalante <danner.escalante.noel@gmail.com>
2026-04-15 23:22:12 -0400

    corrección de la base de datos 2

--- core/database.py
+++ core/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:1234@vitiligo-db:5432/vitiligo_db"
+DATABASE_URL = "postgresql://postgres:1234@vitiligo-db:5434/vitiligo_db"
 
 engine = create_engine(
     DATABASE_URL
