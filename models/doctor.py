3a971994317815c83f479a568a28237bc10acfd3
Danner Escalante <danner.escalante.noel@gmail.com>
2026-04-20 12:20:14 -0400

    arreglando la base de datos

--- models/doctor.py
+++ models/doctor.py
@@ -8,8 +8,6 @@ class Doctor(Base):
     id = Column(Integer, primary_key=True, index=True)
     usuario_id = Column(Integer, ForeignKey("usuarios.id"), unique=True)
     nombre = Column(String)
-    especialidad = Column(String)
-    numero_colegiatura = Column(String)
 
     usuario = relationship("Usuario")
     historiales = relationship("HistorialClinico", back_populates="doctor")
