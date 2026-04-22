2a0abc91683ab9bcf36c2b1093bec28c34f6ba27
Danner Escalante <danner.escalante.noel@gmail.com>
2026-04-22 12:29:35 -0400

    actualizando historial

--- models/historial_clinico.py
+++ models/historial_clinico.py
@@ -24,3 +24,4 @@ class HistorialClinico(Base):
     cita = relationship("Cita", back_populates="historial")
     tratamiento_id = Column(Integer, ForeignKey("tratamientos.id"), nullable=True)
     tratamiento = relationship("Tratamiento", back_populates="historiales")
+    
