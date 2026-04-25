7c1e31a60a5c04c9956e24f863e859dbed56cdf4
Danner Escalante <danner.escalante.noel@gmail.com>
2026-04-25 19:44:22 -0400

    Recargar base de datos

--- core/database.py
+++ core/database.py
@@ -4,7 +4,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 DATABASE_URL = "postgresql://postgres:1234@vitiligo-db:5432/vitiligo_db"
 
 engine = create_engine(
-    DATABASE_URL
+    DATABASE_URL,
+    pool_pre_ping=True
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
