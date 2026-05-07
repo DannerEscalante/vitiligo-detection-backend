161754bde6685cc8a065eb5c62e1abdf1fcf8bfe
Danner Escalante <danner.escalante.noel@gmail.com>
2026-05-06 20:25:27 -0400

    arreglando conexion 3

--- core/database.py
+++ core/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:backend_test%21@vitiligo-db:5432/vitiligo_db"
+DATABASE_URL = "postgresql://postgres:Vit1l1g0_S3cur3@vitiligo-db:5432/vitiligo_db"
 
 engine = create_engine(
     DATABASE_URL,
